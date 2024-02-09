c051236b27ca2dda4103c58ca238bcf8e98f8950
Akshay Gaikwad <akshayakgaikwad@gmail.com>
2024-02-09 16:22:43 +0530

    First commit

---
+++ rq-design/src/main/java/com/example/rqdesign/RqDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.rqdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RqDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RqDesignApplication.class, args);
+	}
+
+}
